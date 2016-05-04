fb33d854e71d355d6e493fdf0e8ce56636ff679b
Rémi Duraffort <ivoire@videolan.org>
2016-05-04 15:36:02 +0200

    Improve log formating

--- reactobus.py
+++ reactobus.py
@@ -10,7 +10,7 @@ import yaml
 from lib.core import Core
 
 
-FORMAT = "%(asctime)-15s %(levelname)s %(name)s %(message)s"
+FORMAT = "%(asctime)-15s %(levelname)7s %(name)s %(message)s"
 LOG = logging.getLogger("ROB")
 
 
