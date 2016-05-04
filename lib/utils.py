44ad0b6e5c166ef1b0eeb85a8862fff42f7beca6
Rémi Duraffort <ivoire@videolan.org>
2016-05-04 16:57:54 +0200

    Recursiv lookup on subclasses

--- lib/utils.py
+++ lib/utils.py
@@ -6,10 +6,20 @@ class Pipe(multiprocessing.Process):
 
     @classmethod
     def select(cls, classname, name, options, inbound):
-        for sub in cls.__subclasses__():
+        for sub in cls.subclasses():
             if sub.classname == classname:
                 return sub(name, options, inbound)
         raise NotImplementedError
+
+    @classmethod
+    def subclasses(cls):
+        subcls = []
+        for sub in cls.__subclasses__():
+            subcls.append(sub)
+            child = sub.subclasses()
+            if child is not []:
+                subcls.extend(child)
+        return subcls
 
     def setup(self):
         raise NotImplementedError
