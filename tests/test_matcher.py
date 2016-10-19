055c68439c3209480f3a5e73fc577a4cdc7b93ad
Rémi Duraffort <ivoire@videolan.org>
2016-10-19 10:17:49 +0200

    Add missing license header

--- tests/test_matcher.py
+++ tests/test_matcher.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+# vim: set ts=4
+
+# Copyright 2016 Rémi Duraffort
+# This file is part of ReactOBus.
+#
+# ReactOBus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ReactOBus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with ReactOBus.  If not, see <http://www.gnu.org/licenses/>
+
 import pytest
 
 from lib.reactor import Matcher
