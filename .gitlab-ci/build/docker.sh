c24c03885a7c17d888c8030a9447407263486b8b
Rémi Duraffort <remi.duraffort@linaro.org>
2019-07-15 12:38:15 +0200

    CI: set the right version when building the docker image
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- .gitlab-ci/build/docker.sh
+++ .gitlab-ci/build/docker.sh
@@ -6,9 +6,16 @@ if [ "$1" = "setup" ]
 then
   set -x
   docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
+  apk add git
 else
   set -x
 
+  # Patch the version on the fly
+  VERSION=$(git describe)
+  sed -i "s#^__version__ = .*\$##" reactobus/__about__.py
+  echo "__version__ = $VERSION" >> reactobus/__about__.py
+
+  # Build the docker image
   docker build -t $CI_REGISTRY_IMAGE:latest .
 
   # Push only for tags or master
